787678b4db00d839ae705a560a3c42551d517d65
krishna murari <krishnamutari160@gmail.com>
2025-11-03 15:11:45 +0530

    Hooks usestate

---
+++ shopping-vite-App/src/components/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Book.css'
+
+function Book({ book }) {
+  return (
+    <div className='card'>
+      <img src={book.image} width={200} height={200} alt={book.title} />
+      <h3>Title: {book.title}</h3>
+      <h4>Price: ₹{book.price}</h4>
+      <button>Add To Cart</button>
+    </div>
+  )
+}
+
+export default Book
